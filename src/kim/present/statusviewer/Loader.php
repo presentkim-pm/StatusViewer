aa3add095c94ddb42ada264ba94a1f4b0fca78ca
PresentKim <debe3721@gmail.com>
2021-02-19 07:12:48 +0900

    Init StatusViewer plugin

---
+++ src/kim/present/statusviewer/Loader.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace kim\present\statusviewer;
+
+use pocketmine\plugin\PluginBase;
+
+final class Loader extends PluginBase{
+    protected function onEnable() : void{
+    }
+}
